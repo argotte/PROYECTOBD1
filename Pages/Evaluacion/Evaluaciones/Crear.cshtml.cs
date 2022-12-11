522b827711993ed4c290333afe0039c95407a986
helljound <rikijmg98@gmail.com>
2022-12-11 06:38:08 -0400

    222

--- Pages/Evaluacion/Evaluaciones/Crear.cshtml.cs
+++ Pages/Evaluacion/Evaluaciones/Crear.cshtml.cs
@@ -3,46 +3,48 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using PROYECTOBD1.Modelos;
 using System.Data.SqlClient;
 
-namespace PROYECTOBD1.Pages.Clientes
+namespace PROYECTOBD1.Pages.Evaluacion.Evaluaciones
 {
     public class CrearModel : PageModel
     {
         String connectionString = "Data Source=DESKTOP-P186VBB;Initial Catalog=ProyectoCereza;Persist Security Info=True;User ID=sa;Password=12345678";
-        public List<PaisModelo> listaPaises = new List<PaisModelo>();
-        public List<CiudadModelo> listaCiudades = new List<CiudadModelo>();
-        public ClienteModelo clienteModelo = new ClienteModelo();
+        public List<ClienteModelo> listaClientes = new List<ClienteModelo>();
+        public EvaluacionModelo evaluacion = new EvaluacionModelo();
         public string error = "";
-        public string correcto="";
-        public string pais, ciudad;
-
+        public string correcto = "";
+        public string cliente;
         public void OnGet()
         {
-
         }
+
         public void OnPost()
         {
-            clienteModelo.NOMBRE = Request.Form["NOMBRE"];
-            clienteModelo.FK_ID_PAIS = Request.Form["FK_ID_PAIS"];
-            clienteModelo.FK_ID_CIUDAD = Request.Form["FK_ID_CIUDAD"];
-            if (clienteModelo.FK_ID_PAIS.Length == 0 || clienteModelo.FK_ID_PAIS.Length == 0 || clienteModelo.NOMBRE.Length == 0)
+            evaluacion.ANIO = Request.Form["ANIO"];
+            evaluacion.FECHAEVALUACION = Request.Form["FECHAEVALUACION"];
+            evaluacion.DECISIONFINAL = Request.Form["DECISIONFINAL"];
+            evaluacion.RESULTADO = Request.Form["RESULTADO"];
+            evaluacion.PORCENTAJE_RESULTADO = Request.Form["PORCENTAJE_RESULTADO"];
+            evaluacion.FK_ID_CLIENTE = Request.Form["FK_ID_CLIENTE"];
+            evaluacion.FK_ID_PRODUCTOR = Request.Form["FK_ID_PRODUCTOR"];
+            if (evaluacion.FK_ID_CLIENTE.Length == 0 || evaluacion.FK_ID_PRODUCTOR.Length == 0 || evaluacion.ANIO.Length == 0 || evaluacion.FECHAEVALUACION.Length == 0 || evaluacion.DECISIONFINAL.Length == 0 || evaluacion.RESULTADO.Length == 0 || evaluacion.PORCENTAJE_RESULTADO.Length == 0)
             {
                 error = "TODOS LOS CAMPOS SON REQUERIDOS";
                 return;
             }
             try
             {
-                using (SqlConnection connection = new SqlConnection(connectionString)) 
-                { 
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
                     connection.Open();
                     String sql = "SELECT P.ID,C.ID FROM DJR_PAISES AS P INNER JOIN DJR_CIUDADES C ON P.ID=C.FK_ID_PAIS WHERE P.NOMBRE= @PAIS AND C.NOMBRE= @CIUDAD";
                     using (SqlCommand command = new SqlCommand(sql, connection))
-                    {            
+                    {
                         command.Parameters.AddWithValue("@PAIS", clienteModelo.FK_ID_PAIS);
                         command.Parameters.AddWithValue("@CIUDAD", clienteModelo.FK_ID_CIUDAD);
-                       // command.ExecuteNonQuery();
+                        // command.ExecuteNonQuery();
                         using (SqlDataReader reader = command.ExecuteReader())
                         {
-                            while (reader.Read()) 
+                            while (reader.Read())
                             {
                                 pais = "" + reader.GetInt32(0);
                                 ciudad = "" + reader.GetInt32(1);
@@ -64,14 +66,14 @@ namespace PROYECTOBD1.Pages.Clientes
                         command.Parameters.AddWithValue("@FK_ID_CIUDAD", ciudad);
                         command.ExecuteNonQuery();
                     }
-                    
+
                 }
             }
             catch (Exception ex)
             {
                 error = ex.Message;
                 return;
-               
+
             }
             //ahora salva la info en la bd
             clienteModelo.NOMBRE = ""; clienteModelo.FK_ID_CIUDAD = ""; clienteModelo.FK_ID_PAIS = "";
---
+++ Pages/Evaluacion/Evaluaciones/Mostrar.cshtml.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PROYECTOBD1.Modelos;
+using System.Data.SqlClient;
+
+namespace PROYECTOBD1.Pages.Evaluacion.Evaluaciones
+{
+    public class MostrarModel : PageModel
+    {
+        public List<EvaluacionModelo> listaEva = new List<EvaluacionModelo>();
+        String connectionString = "Data Source=DESKTOP-P186VBB;Initial Catalog=ProyectoCereza;Persist Security Info=True;User ID=sa;Password=12345678";
+        public void OnGet()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT E.ANIO,E.FECHAEVALUACION, E.DECISIONFINAL,E.RESULTADO,E.PORCENTAJE_RESULTADO,E.FK_ID_PRODUCTOR, E.FK_ID_CLIENTE FROM DJR_EVALUACIONES E";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                EvaluacionModelo evaluacion = new EvaluacionModelo();
+                                evaluacion.ANIO= (reader.IsDBNull(0) != true) ? "" + reader.GetString(0) : "";
+                                evaluacion.FECHAEVALUACION = (reader.IsDBNull(1) != true) ? "" + reader.GetDateTime(1) : "";
+                                evaluacion.DECISIONFINAL = (reader.IsDBNull(2) != true) ? "" + reader.GetString(2) : "";
+                                evaluacion.RESULTADO = (reader.IsDBNull(3) != true) ? "" + reader.GetString(3) : "";
+                                evaluacion.PORCENTAJE_RESULTADO = (reader.IsDBNull(4) != true) ? "" + reader.GetDecimal(4) : "";
+                                evaluacion.FK_ID_PRODUCTOR = (reader.IsDBNull(5) != true) ? "" + reader.GetInt32(5) : "";
+                                evaluacion.FK_ID_CLIENTE = (reader.IsDBNull(6) != true) ? "" + reader.GetInt32(6) : "";
+                                //       paisModelo.Ciudades= new List<CiudadModelo>();
+                                listaEva.Add(evaluacion);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
