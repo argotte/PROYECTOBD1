c953ee643bd9fa46410b78b322fb5f48648468d2
helljound <rikijmg98@gmail.com>
2022-12-11 02:10:43 -0400

    MODELOS COMPLETOS

--- Modelos/FormulaModelo.cs
+++ Modelos/FormulaModelo.cs
@@ -2,6 +2,15 @@
 {
     public class FormulaModelo
     {
+        public string FK_ID_CRITERIO { get; set; }
+        public string FK_ID_CLIENTE { get; set; }
+        public string ID { get; set; }
+        public string TIPO { get; set; }
+        public string PORCENTAJE_IMPORTANCIA { get; set; }
+        public string PORCENTAJE_ACEPTACION { get; set; }
 
+        //////////////////////////////
+        public string NOMBRECRITERIO { get; set; }
+        public string NOMBRECLIENTE { get; set; }
     }
 }
