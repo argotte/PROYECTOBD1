268678908730592413f6d5b934e456aa93c18d3b
helljound <rikijmg98@gmail.com>
2022-12-11 02:19:48 -0400

    listo con el lobby de evaluacion

---
+++ Pages/Evaluacion/Lobby.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PROYECTOBD1.Pages.Evaluacion
+{
+    public class LobbyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
